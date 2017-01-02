856a7249b2a9edae0133c9ceac175601252a2f11
Prunaru Cosmin-Nicolae <prunarucosmin@gmail.com>
2017-01-03 00:24:44 +0200

    Inheritance II
    Second Inheritance HackerRank exercise.

---
+++ CSharpInheritance II/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInheritance_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adder adder = new Adder();
+
+            Console.WriteLine("My base is: {0}", adder.GetType().BaseType);
+            Console.WriteLine("10 + 3 = {0}", adder.add(10, 3));
+
+            Console.ReadKey();
+        }
+    }
+}
