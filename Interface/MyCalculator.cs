d718c03a8c956ce06f4c43a519a71075c0153409
Prunaru Cosmin-Nicolae <prunarucosmin@gmail.com>
2017-01-03 01:06:42 +0200

    Interface
    Solved the interface exercise

---
+++ Interface/MyCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class MyCalculator : IArithmetic
+    {
+        public int divisorSum(int n)
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= n / 2; i++)
+                if (n % i == 0)
+                    sum += i;   
+            
+            sum += n;
+            return sum;
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 13;
+            MyCalculator calculator = new MyCalculator();
+            Console.WriteLine("I inherited: ");
+            ImplementedInterfaces(calculator);
+
+            Console.WriteLine("The sum of number {0}'s divisors is {1}", number, calculator.divisorSum(number));
+            Console.ReadKey();
+        }
+
+        static void ImplementedInterfaces(Object o)
+        {
+            Type[] interfaces = o.GetType().GetInterfaces();
+            foreach (var name in interfaces)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
