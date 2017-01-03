49501cef750c5251be868adcb80e6a8557509653
Prunaru Cosmin-Nicolae <prunarucosmin@gmail.com>
2017-01-03 13:06:37 +0200

    Override II
    Implementation for second override exercise

---
+++ Override II/Bicycle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Override_II
+{
+    class Bicycle
+    {
+        public String define_me()
+        {
+            return "a vehicle with pedals";
+        }
+    }
+}
---
+++ Override II/Motorcycle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Override_II
+{
+    class Motorcycle : Bicycle
+    {
+        public Motorcycle()
+        {
+            Console.WriteLine("Hello, I am a motorcycle, I am {0}", define_me());
+
+            String temp = base.define_me();
+
+            Console.WriteLine("My ancestor is a cycle who is {0}", temp);
+        }
+
+        public new String define_me()
+        {
+            return "a cycle with an engine";
+        }
+    }
+}
